44ba74e870736f29d28418587c2448137a555764
hardzal <suryadijogja@gmail.com>
2025-07-11 23:25:10 +0700

    update to input text

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { configDotenv } from "dotenv";
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
 
 configDotenv();
 
@@ -7,13 +9,29 @@ const API_KEY = process.env.GOOGLE_API_KEY || "";
 
 const genAI = new GoogleGenerativeAI(API_KEY);
 
-const prompt_text = "Tell, what's you know about valorant?";
-
-console.log(prompt_text);
-
 (async () => {
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-  const { response } = await model.generateContent(prompt_text);
-  console.log("Hello, Gemini here!");
-  console.log(response.text());
+
+  const input_text = readline.createInterface({
+    input,
+    output,
+  });
+
+  const prompt_text = await input_text.question(
+    "Hello, Gemini here!, How can I help you? "
+  );
+
+  // menampilkan secara langsung text keseluruhan
+  //   const { response } = await model.generateContent(prompt_text);
+  //   console.log(response.text());
+
+  // menampilkan teks secara bertahap (Typing text)
+  const newResponse = await model.generateContentStream(prompt_text);
+  for await (const chunk of newResponse.stream) {
+    const chunkText = chunk.text();
+    process.stdout.write(chunkText);
+  }
+
+  console.log();
+  input_text.close();
 })();
